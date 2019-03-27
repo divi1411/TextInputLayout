14da55bc700c0afa9cb6b63b1feb19a0798d3b45
Divya Venkatesan <divya.venkatesan@syncfusion.com>
2019-03-27 18:29:47 +0530

    Sample

---
+++ TextInputLayout/TextInputLayout.UWP/CustomTemplatedViewRenderer.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextInputLayout;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms.Platform.UWP;
+using Forms = Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(CustomTemplatedView), typeof(TextInputLayout.UWP.CustomTemplatedViewRenderer))]
+namespace TextInputLayout.UWP
+{
+    public class CustomTemplatedViewRenderer : ViewRenderer<CustomTemplatedView, FrameworkElement>
+    {
+        CustomTemplatedView customTemplatedView;
+
+        TextBox nativeTextBox;
+
+        private string textBoxText = string.Empty;
+
+        private bool isInternalChange;
+        protected override void OnElementChanged(ElementChangedEventArgs<CustomTemplatedView> e)
+        {
+            base.OnElementChanged(e);
+
+            var element = e.NewElement;
+            if (element != null && element is CustomTemplatedView)
+            {
+                customTemplatedView = element as CustomTemplatedView;
+                UpdateNativeView(customTemplatedView.InputView);
+                if (nativeTextBox != null)
+                {
+                    nativeTextBox.Padding = new Thickness(0, 24, 0, 10);
+                }
+            }
+        }
+
+        private void UpdateNativeView(Forms.View view)
+        {
+            if (nativeTextBox != null)
+            {
+                nativeTextBox.TextChanged -= NativeTextBox_TextChanged;
+                nativeTextBox.Loaded -= NativeTextBox_Loaded;
+            }
+
+            if (view == null)
+            {
+                return;
+            }
+
+            if (Platform.GetRenderer(view) == null)
+            {
+                Platform.SetRenderer(view, Platform.CreateRenderer(view));
+            }
+
+            var renderer = Platform.GetRenderer(view);
+            if (renderer is Panel)
+            {
+                var panel = renderer as Panel;
+                foreach (var child in panel.Children)
+                {
+                    if (child is FormsTextBox)
+                    {
+                        (child as FormsTextBox).BackgroundFocusBrush = new SolidColorBrush(Colors.Transparent);
+                    }
+
+                    if (child is TextBox)
+                    {
+                        nativeTextBox = child as TextBox;
+                        if (nativeTextBox != null)
+                        {
+                            nativeTextBox.IsTabStop = customTemplatedView.IsEnabled;
+                            textBoxText = nativeTextBox.Text;
+                            isInternalChange = !string.IsNullOrEmpty(textBoxText);
+                            if (isInternalChange)
+                            {
+                                nativeTextBox.Text = string.Empty;
+                            }
+
+                            if (nativeTextBox != null)
+                            {
+                                nativeTextBox.TextChanged += NativeTextBox_TextChanged;
+                                nativeTextBox.Loaded += NativeTextBox_Loaded;
+                            }
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void NativeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isInternalChange && nativeTextBox.Text == textBoxText)
+            {
+                isInternalChange = false;
+                return;
+            }
+        }
+
+        private void NativeTextBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            var childCount = VisualTreeHelper.GetChildrenCount(nativeTextBox);
+            for (int i = 0; i < childCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(nativeTextBox, i);
+                if (child is Grid)
+                {
+                    var parent = child as Grid;
+                    if (parent == null)
+                    {
+                        continue;
+                    }
+
+                    var clearButton = parent.FindName("DeleteButton") as Button;
+                    if (clearButton != null)
+                    {
+                        clearButton.Visibility = Visibility.Collapsed;
+                        clearButton.Style = null;
+                        (clearButton.Parent as Panel)?.Children.Remove(clearButton);
+                    }
+
+                    var borderElement = parent.FindName("BorderElement") as Border;
+                    if (borderElement != null)
+                    {
+                        borderElement.Visibility = Visibility.Collapsed;
+                    }
+
+                    var backgroundElement = parent.FindName("BackgroundElement") as Border;
+                    if (backgroundElement != null)
+                    {
+                        backgroundElement.Visibility = Visibility.Collapsed;
+                    }
+
+                    UpdateText();
+                    //Forms.Device.BeginInvokeOnMainThread(() => UpdateText());
+
+                    break;
+                }
+            }
+         
+        }
+
+        void UpdateText()
+        {
+            if (this.isInternalChange)
+            {
+                var currentText = nativeTextBox.Text;
+                textBoxText = string.IsNullOrEmpty(currentText) ? textBoxText : currentText;
+                nativeTextBox.Text = textBoxText;
+            }
+        }
+    }
+}
---
+++ TextInputLayout/TextInputLayout/CustomTemplatedView.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TextInputLayout
+{
+    [ContentProperty("InputView")]
+    public class CustomTemplatedView : TemplatedView
+    {
+
+        public static readonly BindableProperty InputViewProperty =
+            BindableProperty.Create("InputView", typeof(View), typeof(CustomTemplatedView), null, BindingMode.Default, null, OnInputViewChanged);
+        public View InputView
+        {
+            get { return (View)GetValue(InputViewProperty); }
+            set { SetValue(InputViewProperty, value); }
+        }
+
+        Grid grid;
+        public CustomTemplatedView()
+        {
+            this.ControlTemplate = new ControlTemplate(typeof(StackLayout));
+            ((StackLayout)Children[0]).Children.Add(grid = new Grid());
+
+            this.grid.RowDefinitions = new RowDefinitionCollection
+            {
+                new RowDefinition { Height = new GridLength(0, GridUnitType.Absolute) },
+                new RowDefinition { Height = GridLength.Auto },
+            };
+
+            grid.RowSpacing = 0;
+            grid.ColumnSpacing = 0;
+        }
+
+
+        private static void OnInputViewChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            (bindable as CustomTemplatedView).OnInputViewChanged(oldValue, newValue);
+        }
+
+        private void OnInputViewChanged(object oldValue, object newValue)
+        {
+            var oldView = (View)oldValue;
+            if (oldView != null)
+            {
+                if (this.grid.Children.Contains(oldView))
+                {
+                    oldView.BindingContext = null;
+                    this.grid.Children.Remove(oldView);
+                }
+            }
+
+            var newView = (View)newValue;
+            if (newView != null)
+            {
+                this.grid.Children.Add(newView, 0, 1);
+            }
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            if (grid != null)
+            {
+                SetInheritedBindingContext(this.grid, this.BindingContext);
+            }
+            base.OnBindingContextChanged();
+
+        }
+    }
+}
